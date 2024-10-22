636b8cb3b1f9642ecda71ac6229b1b0e89a587ac
Samir <abakhri.sd@gmail.com>
2024-10-22 21:18:06 +0500

    tuples

---
+++ tuples.ts
@@ -0,0 +1,8 @@
+const skill: [number, string] = [1, 'Dev'];
+const id = skill[0];
+const skillName = skill[1];
+// const w00t = skill[2];
+
+const [idD, skillNameD] = skill;
+
+const extendedTuple: [number, string, ...boolean[]] = [1, 'dva', true, false];
