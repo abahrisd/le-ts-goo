0f36bb6333e2eba6c0aba1f3ba0aa42a1d3cdc40
Samir <abakhri.sd@gmail.com>
2024-10-22 21:21:40 +0500

    readonly

---
+++ ro.ts
@@ -0,0 +1,4 @@
+const arr: readonly [number, string] = [1, 'dev'];
+
+// arr[0] = 2;
+// arr.push(3);
---
+++ tuples.js
@@ -0,0 +1,7 @@
+"use strict";
+const skill = [1, 'Dev'];
+const id = skill[0];
+const skillName = skill[1];
+// const w00t = skill[2];
+const [idD, skillNameD] = skill;
+const extendedTuple = [1, 'dva', true, false];
