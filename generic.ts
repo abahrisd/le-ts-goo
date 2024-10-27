1cca3b76bd695adcdfefa7bf4725b49b206a07ec
Samir <abakhri.sd@gmail.com>
2024-10-28 00:15:05 +0500

    generic class

--- generic.ts
+++ generic.ts
@@ -60,3 +60,32 @@ function logIdGen<T extends string | number, Y>(id: T, data: Y) {
     console.log('data', data);
     return id;
 }
+
+/////////////////////////////
+
+class Resp<D, E> {
+    data?: D;
+    error?: E;
+
+    constructor(data?: D, error?: E) {
+        if (data) {
+            this.data = data;
+        }
+
+        if (error) {
+            this.error = error;
+        }
+    }
+}
+
+const resGen = new Resp<string, number>('data');
+
+class HTTPResp<F> extends Resp<string, number>{
+    code: F;
+
+    setCode(code: F) {
+        this.code = code;
+    }
+}
+
+const resp2Gen = new HTTPResp();
