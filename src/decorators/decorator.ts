7d1c97915d32a84c7f36c249fd51e25b34b788ba
Samir <abakhri.sd@gmail.com>
2024-10-30 14:32:08 +0500

    decorators factory

--- src/decorators/decorator.ts
+++ src/decorators/decorator.ts
@@ -2,9 +2,13 @@ interface IUserService {
     users: number;
     getUsersInDatabase(): number;
 }
+// decorators initialise from top to down, but run from down to top
 
 // @nullUsers
-@threeUsersAdvanced
+// @threeUsersAdvanced
+// @setUsers(2)
+@setUsersAdvanced(2)
+@log()
 class UserServiceDec implements IUserService {
     users: number;
 
@@ -18,10 +22,34 @@ function nullUsers(target: Function) {
     target.prototype.users = 0;
 }
 
+function setUsers(number: number) {
+    console.log('setUsers init',)
+    return (target: Function) => {
+        console.log('setUsers run',)
+        target.prototype.users = number;
+    }
+}
+
 function threeUsersAdvanced<T extends {new(...args: any[]) : {}}>(constructor: T) {
     return class extends constructor {
         users = 3;
     }
 }
+
+function setUsersAdvanced(number: number) {
+    return <T extends {new(...args: any[]) : {}}>(constructor: T) => {
+        return class extends constructor {
+            users = number;
+        }
+    }
+}
+
+function log() {
+    console.log('Log init',)
+    return (target: Function) => {
+        console.log('Log run',target);
+    }
+}
+
 
 console.log(new UserServiceDec().getUsersInDatabase());
